ee467dd0df22ea8f678df4b71963fae95e48900a
Sulayman Yaffa <sulayaffa@gmail.com>
2022-09-05 14:35:11 -0700

    Update customer_data.sql

--- string_manipulation/customer_data.sql
+++ string_manipulation/customer_data.sql
@@ -45,3 +45,25 @@ JOIN
 WHERE
     [City] = 'Seattle'
 
+-- Let's find the customers that are not located in the United States
+SELECT
+    [FirstName],
+    [MiddleName],
+    [LastName],
+    [EmailAddress],
+    [CountryRegion],
+    [AddressLine1],
+    [AddressLine2],
+    [City],
+    [StateProvince],
+    [AddressType]
+FROM
+    [SalesLT].[Customer] c
+JOIN
+    [SalesLT].[CustomerAddress] ca
+    ON  c.CustomerID = ca.CustomerID
+JOIN
+    [SalesLT].[Address] a
+    ON ca.AddressID = a.AddressID
+WHERE
+    [CountryRegion] <> 'United States'  -- I noticed there are two other countries where are customers are from The United Kingdom and Canada
