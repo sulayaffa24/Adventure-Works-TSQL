2820d5445bd369255bd00de1294f71e6290bcd37
Sulayman Yaffa <sulayaffa@gmail.com>
2022-09-05 15:21:26 -0700

    Update customer_data.sql

--- string_manipulation/customer_data.sql
+++ string_manipulation/customer_data.sql
@@ -21,6 +21,7 @@ JOIN
 WHERE
     [CountryRegion] = 'United States'
     
+    
 -- Let's find all the customers who live in the city 'Seattle'
 
 SELECT
@@ -44,6 +45,7 @@ JOIN
     ON ca.AddressID = a.AddressID
 WHERE
     [City] = 'Seattle'
+    
 
 -- Let's find the customers that are not located in the United States
 SELECT
@@ -67,3 +69,49 @@ JOIN
     ON ca.AddressID = a.AddressID
 WHERE
     [CountryRegion] <> 'United States'  -- I noticed there are two other countries where are customers are from The United Kingdom and Canada
+    
+-- How many customers were from The United Kingdom
+SELECT
+    [FirstName],
+    [MiddleName],
+    [LastName],
+    [EmailAddress],
+    [CountryRegion],
+    [AddressLine1],
+    [AddressLine2],
+    [City],
+    [StateProvince],
+    [AddressType]
+FROM
+    [SalesLT].[Customer] c
+JOIN
+    [SalesLT].[CustomerAddress] ca
+    ON  c.CustomerID = ca.CustomerID
+JOIN
+    [SalesLT].[Address] a
+    ON ca.AddressID = a.AddressID
+WHERE
+    [CountryRegion] = 'United Kingdom'
+
+-- Which City from The United Kingdom showed up the most from the dataset
+SELECT
+    COUNT(*) AS number_of_times,
+    [CountryRegion],
+    [City],
+    [StateProvince],
+    [AddressType]
+FROM
+    [SalesLT].[Customer] c
+JOIN
+    [SalesLT].[CustomerAddress] ca
+    ON  c.CustomerID = ca.CustomerID
+JOIN
+    [SalesLT].[Address] a
+    ON ca.AddressID = a.AddressID
+WHERE
+    [CountryRegion] = 'United Kingdom'
+GROUP BY 
+    [CountryRegion],
+    [City],
+    [StateProvince],
+    [AddressType]      -- Most of the customers were from London
