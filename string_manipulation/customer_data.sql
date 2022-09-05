fd853a473fb92afaccf3583abbf50a72ff9ab33f
Sulayman Yaffa <sulayaffa@gmail.com>
2022-09-05 14:23:20 -0700

    Update customer_data.sql

--- string_manipulation/customer_data.sql
+++ string_manipulation/customer_data.sql
@@ -1,3 +1,26 @@
+-- Let's find the customers located in the United States
+SELECT
+    [FirstName],
+    [MiddleName],
+    [LastName],
+    [EmailAddress],
+    [CountryRegion],
+    [AddressLine1],
+    [AddressLine2],
+    [City],
+    [StateProvince],
+    [AddressType]
+FROM
+    [SalesLT].[Customer] c
+JOIN
+    [SalesLT].[CustomerAddress] ca
+    ON  c.CustomerID = ca.CustomerID
+JOIN
+    [SalesLT].[Address] a
+    ON ca.AddressID = a.AddressID
+WHERE
+    [CountryRegion] = 'United States'
+    
 -- Let's find all the customers who live in the city 'Seattle'
 
 SELECT
