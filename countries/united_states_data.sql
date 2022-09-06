6d5b3613884dfec4bcf4ac2d98f058418eb5baee
Sulayman Yaffa <sulayaffa@gmail.com>
2022-09-05 17:24:58 -0700

    Update united_states_data.sql
    Added queries using the INNER JOIN, WHERE, GROUP BY, and ORDER BY clauses

--- countries/united_states_data.sql
+++ countries/united_states_data.sql
@@ -1,1 +1,68 @@
+-- Let's find the customers located in the United States
+SELECT
+    [FirstName],
+    [MiddleName],
+    [LastName],
+    [EmailAddress],
+    [CountryRegion],
+    [AddressLine1],
+    [AddressLine2],
+    [City],
+    [StateProvince],
+    [AddressType]
+FROM
+    [SalesLT].[Customer] c
+JOIN
+    [SalesLT].[CustomerAddress] ca
+    ON  c.CustomerID = ca.CustomerID
+JOIN
+    [SalesLT].[Address] a
+    ON ca.AddressID = a.AddressID
+WHERE
+    [CountryRegion] = 'United States'
+    
+    
+-- Let's find all the customers who live in the city 'Seattle'
+
+SELECT
+    [FirstName],
+    [MiddleName],
+    [LastName],
+    [EmailAddress],
+    [CountryRegion],
+    [AddressLine1],
+    [AddressLine2],
+    [City],
+    [StateProvince],
+    [AddressType]
+FROM
+    [SalesLT].[Customer] c
+JOIN
+    [SalesLT].[CustomerAddress] ca
+    ON  c.CustomerID = ca.CustomerID
+JOIN
+    [SalesLT].[Address] a
+    ON ca.AddressID = a.AddressID
+WHERE
+    [City] = 'Seattle'
+
+-- How many times did a StateProvince appear in the dataset
+SELECT
+    COUNT(*) AS number_of_times,
+    [StateProvince]
+FROM
+    [SalesLT].[Customer] c
+JOIN
+    [SalesLT].[CustomerAddress] ca
+    ON  c.CustomerID = ca.CustomerID
+JOIN
+    [SalesLT].[Address] a
+    ON ca.AddressID = a.AddressID
+WHERE
+    [CountryRegion] = 'United States'
+GROUP BY 
+    [StateProvince]
+ORDER BY 
+    number_of_times DESC  -- California showed up the most with 65 times followed by Washington with 48
+    
 
