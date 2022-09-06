3553f9f2e6bf6a1f0d5a8a000b855a10c2e1107c
Sulayman Yaffa <sulayaffa@gmail.com>
2022-09-05 21:51:48 -0700

    Update united_kingdom_data.sql
    Adding United Kingdom data

--- countries/united_kingdom_data.sql
+++ countries/united_kingdom_data.sql
@@ -1,1 +1,23 @@
+-- How many customers were from The United Kingdom
+SELECT
+    [FirstName],
+    [MiddleName],
+    [LastName],
+    [EmailAddress],
+    [CountryRegion],
+    [AddressLine1],
+    [AddressLine2],
+    [City],
+    [StateProvince],
+    [AddressType]
+FROM
+    [SalesLT].[Customer] c
+JOIN
+    [SalesLT].[CustomerAddress] ca
+    ON  c.CustomerID = ca.CustomerID
+JOIN
+    [SalesLT].[Address] a
+    ON ca.AddressID = a.AddressID
+WHERE
+    [CountryRegion] = 'United Kingdom'
 
