46bcadbc822c616d9957e0974bf5e2a01178a198
Sulayman Yaffa <sulayaffa@gmail.com>
2022-09-05 22:29:09 -0700

    Update united_kingdom_data.sql

--- countries/united_kingdom_data.sql
+++ countries/united_kingdom_data.sql
@@ -21,3 +21,27 @@ JOIN
 WHERE
     [CountryRegion] = 'United Kingdom'
 
+-- Which City from The United Kingdom showed up the most from the dataset
+SELECT
+    COUNT(*) AS number_of_times,
+    [CountryRegion],
+    [City],
+    [StateProvince],
+    [AddressType]
+FROM
+    [SalesLT].[Customer] c
+JOIN
+    [SalesLT].[CustomerAddress] ca
+    ON  c.CustomerID = ca.CustomerID
+JOIN
+    [SalesLT].[Address] a
+    ON ca.AddressID = a.AddressID
+WHERE
+    [CountryRegion] = 'United Kingdom'
+GROUP BY 
+    [CountryRegion],
+    [City],
+    [StateProvince],
+    [AddressType]      
+ORDER BY
+    number_of_times DESC -- Most of the customers were from London (13)
